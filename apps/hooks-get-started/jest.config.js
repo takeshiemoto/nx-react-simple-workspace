4e23e2f20762ffeed7d9d0df286b23b40bfb0c65
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2020-06-01 23:29:59 +0900

    initial commit

---
+++ apps/hooks-get-started/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'hooks-get-started',
+  preset: '../../jest.config.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/apps/hooks-get-started',
+};
