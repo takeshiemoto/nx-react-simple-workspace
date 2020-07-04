9c5084cee36f4d14e24d2d082a6874dd49fd004c
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2020-07-04 12:18:38 +0900

    dev

--- apps/hooks-get-started/src/app/app.tsx
+++ apps/hooks-get-started/src/app/app.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './app.scss';
-import Heroes from './components/heroes/heroes';
 
 export const App = () => {
-  const title = 'Tour of heroes';
+  const title = 'Hooks get started';
   return (
     <div className="app">
       <h1>{title}</h1>
-      <Heroes />
     </div>
   );
 };
