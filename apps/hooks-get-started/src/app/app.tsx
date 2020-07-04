f42d75ec5961757c0815b1d832741e95624e000b
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2020-07-04 22:48:22 +0900

    feat: add sample

--- apps/hooks-get-started/src/app/app.tsx
+++ apps/hooks-get-started/src/app/app.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './app.scss';
+import SearchResults from './components/search-results/search-results';
 
 export const App = () => {
   const title = 'Hooks get started';
   return (
     <div className="app">
       <h1>{title}</h1>
+      <SearchResults />
     </div>
   );
 };
---
+++ apps/hooks-get-started/src/app/components/search-results/search-results.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+
+import './search-results.scss';
+
+/* eslint-disable-next-line */
+export interface SearchResultsProps {}
+
+export interface SearchResults {
+  hits: any[];
+}
+
+export const SearchResults = (props: SearchResultsProps) => {
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState('react');
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        'https://hn.algolia.com/api/v1/search?query=' + query
+      );
+      const data = (await response.json()) as SearchResults;
+      setData(data);
+    }
+    fetchData();
+  }, [query]);
+
+  return (
+    <>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <ul>
+        {data.hits.map((item) => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default SearchResults;
