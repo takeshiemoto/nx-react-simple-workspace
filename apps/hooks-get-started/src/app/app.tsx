980520e1dc6f40031c0653934d5d8c9f848ebf71
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2020-07-26 22:28:00 +0900

    dev

--- apps/hooks-get-started/src/app/app.tsx
+++ apps/hooks-get-started/src/app/app.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./app.scss";
-import { SearchResults } from "./components/search-results/search-results";
 import { Counter } from "./components/counter/counter";
 import { CounterFunctionProps } from "./components/counter-function-props/counter-function-props";
 import { CounterUseCallbackProps } from "./components/counter-use-callback-props/counter-use-callback-props";
 import { CounterDouble } from "./components/counter-double/counter-double";
 import { CounterUseMemoDouble } from "./components/counter-use-memo-double/counter-use-memo-double";
 import { CounterUseRef } from "./components/counter-use-ref/counter-use-ref";
+import SearchResults from "./components/search-results/search-results";
 
 export const App = () => {
   return (
