5911c587c6ec9065d730f535e8e07b835acd8e8c
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2020-10-06 10:35:04 +0900

    chore: Upgrade Nx

--- apps/hooks-get-started/jest.config.js
+++ apps/hooks-get-started/jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  name: "hooks-get-started",
-  preset: "../../jest.config.js",
+  preset: "../../jest.preset.js",
   transform: {
     "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nrwl/react/plugins/jest",
     "^.+\\.[tj]sx?$": "ts-jest",
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "html"],
   coverageDirectory: "../../coverage/apps/hooks-get-started",
+  globals: { "ts-jest": { tsConfig: "<rootDir>/tsconfig.spec.json" } },
+  displayName: "hooks-get-started",
 };
--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
-  testMatch: ["**/+(*.)+(spec|test).+(ts|js)?(x)"],
-  transform: {
-    "^.+\\.(ts|js|html)$": "ts-jest",
-  },
-  resolver: "@nrwl/jest/plugins/resolver",
-  moduleFileExtensions: ["ts", "js", "html"],
-  coverageReporters: ["html"],
+  projects: [
+    "<rootDir>/apps/hooks-get-started",
+    "<rootDir>/apps/ooui-destination",
+  ],
 };
